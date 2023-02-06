17d87a2a1be58b3d1ed38f21d1173888a49f5e4b
Zeereoreo <wltjrcjf12@naver.com>
2023-02-06 13:25:11 +0900

    miniserver 만들기

--- server/basic-server.js
+++ server/basic-server.js
@@ -4,12 +4,37 @@ const PORT = 4999;
 
 const ip = 'localhost';
 
-const server = http.createServer((request, response) => {
-  console.log(
-    `http request method is ${request.method}, url is ${request.url}`
-  );
-  response.writeHead(200, defaultCorsHeader);
-  response.end('hello mini-server sprints');
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    if (req.url === '/lower') {
+      let data = '';
+      req.on('data', chunk => {
+        data = data + chunk;
+      });
+      req.on('end', () => {
+        data = data.toLowerCase();
+        res.writeHead(201, defaultCorsHeader);
+        res.end(data);
+      });
+    } else if (req.url === '/upper') {
+      let data = '';
+      req.on('data', chunk => {
+        data = data + chunk;
+      });
+      req.on('end', () => {
+        data = data.toUpperCase();
+        res.writeHead(201, defaultCorsHeader);
+        res.end(data);
+      });
+    } else {
+      res.writeHead(404, defaultCorsHeader);
+      res.end();
+    }
+  }
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, defaultCorsHeader);
+    res.end();
+  }
 });
 
 server.listen(PORT, ip, () => {
