24e1a4c6fb82c8a7279aa9c496065085d2505502
Zeereoreo <wltjrcjf12@naver.com>
2023-02-07 13:32:42 +0900

    express logger

--- Express/App.js
+++ Express/App.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!')
-})
+// app.get('/',(req,res)=>{
+//     res.send('Hello World!')
+// })
 
-app.listen(port,()=>[
-    console.log(`Example app listening on port ${port}`)
-])
+// app.listen(port,()=>[
+//     console.log(`Example app listening on port ${port}`)
+// ])
+
+const myLogger = function (req,res,next) {
+    console.log('LOGGED');
+    next();
+}
+
+app.use(myLogger);
+
+app.get('/', function(req,res){
+    res.send('Hello World@');
+});
+
+app.listen(3000);
