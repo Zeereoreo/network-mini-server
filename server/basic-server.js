0f46fbcf40e0616fc961890ccaccf53160b569ce
Zeereoreo <wltjrcjf12@naver.com>
2023-02-07 14:01:38 +0900

    Express 리팩토링

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,30 +1,59 @@
 const http = require('http');
-
 const PORT = 4999;
-
 const ip = 'localhost';
+const cors = require('cors');
+const router = express.Router()
+const express = require('express')
+const app = express()
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'OPTIONS'){
-    res.writeHead(200, defaultCorsHeader)
-    res.end()
-  }
-  if (req.method === 'POST'){
-    let body = []
-    req.on('data',chunk => {
-      body.push(chunk)
-    }).on('end',()=>{
-      body = Buffer.concat(body).toString();
-      res.writeHead(201, defaultCorsHeader);
-      if (req.url === '/upper') res.end(body.toUpperCase())
-      if (req.url === '/lower') res.end(body.toLowerCase())
-      else{
-        res.writeHead(404, defaultCorsHeader)
-        res.end();
-      }
-    })
-  }
+app.use(cors());
+app.use(express.json({strict : false}))
+
+app.post('/upper', function (req,res){
+  let result = req.body;
+  result = result.toUpperCase();
+  res.json(result)
 })
+
+app.post('/lower', function (req,res){
+  let result = req.body;
+  result = result.toLowerCase();
+  res.json(result)
+})
+
+app.use(function(req,res,next){
+  res.status(404).send('404 Err')
+})
+
+app.use(function(err,req,res, next){
+  console.err(err.stack)
+  res.status(500).send('500 Err')
+})
+
+app.listen(PORT,ip,()=>{
+  console.log(`http server listen on ${ip}:${PORT}`);
+})
+// const server = http.createServer((req, res) => {
+//   if (req.method === 'OPTIONS'){
+//     res.writeHead(200, defaultCorsHeader)
+//     res.end()
+//   }
+//   if (req.method === 'POST'){
+//     let body = []
+//     req.on('data',chunk => {
+//       body.push(chunk)
+//     }).on('end',()=>{
+//       body = Buffer.concat(body).toString();
+//       res.writeHead(201, defaultCorsHeader);
+//       if (req.url === '/upper') res.end(body.toUpperCase())
+//       if (req.url === '/lower') res.end(body.toLowerCase())
+//       else{
+//         res.writeHead(404, defaultCorsHeader)
+//         res.end();
+//       }
+//     })
+//   }
+// })
 //   if (req.method === 'POST') {
 //     if (req.url === '/lower') {
 //       let data = '';
@@ -57,13 +86,13 @@ const server = http.createServer((req, res) => {
 //   }
 // });
 
-server.listen(PORT, ip, () => {
-  console.log(`http server listen on ${ip}:${PORT}`);
-});
+// server.listen(PORT, ip, () => {
+//   console.log(`http server listen on ${ip}:${PORT}`);
+// });
 
-const defaultCorsHeader = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Accept',
-  'Access-Control-Max-Age': 10
-};
+// const defaultCorsHeader = {
+//   'Access-Control-Allow-Origin': '*',
+//   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'Access-Control-Allow-Headers': 'Content-Type, Accept',
+//   'Access-Control-Max-Age': 10
+// };
